6624287a35fe24a9506b81a3f27e3e035208e77a
luxomattic <michiels.thomas@gmail.com>
2012-07-06 09:33:08 +0000

    BreakinEggs Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BreakinEggs::Application.config.secret_token = 'e6328eb33be9fe89a64e6f27187f2f222ca457709d36c553777785901cf2ed774e8aa5ae2e1a86caa1800fc79787c35cce202929048587c7b563afe6de044a9d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BreakinEggs::Application.config.session_store :cookie_store, :key => '_BreakinEggs_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BreakinEggs::Application.config.session_store :active_record_store
